b945049ca8cba77f7d4182f29332923dca5fae8b
youngje jo <siosio3103@gmail.com>
2020-07-15 20:41:48 +0900

    Make frontend skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { Provider, useSelector } from "react-redux";
+import configureStore, { RootState } from "./store/configureStore";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-import "./App.css";
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "store/rootReducer";
+import RootPage from "pages/root";
+import AuthRoute from "components/authRoute";
 
 function App() {
+  const store = configureStore();
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <RootPage>
+          <Switch></Switch>
+        </RootPage>
+      </ConnectedRouter>
+    </Provider>
   );
 }
 
