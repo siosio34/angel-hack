dc2305ededeec384e3b76f70700663041bad82f7
youngje jo <siosio3103@gmail.com>
2020-07-19 14:46:25 +0900

    로그인페이지제작

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,9 +1,73 @@
 import React from "react";
 
-interface Props {}
+import { Form } from "antd";
+import { Store } from "antd/lib/form/interface";
+import { AccountActionCreators } from "store/account/action";
+import { useDispatch } from "react-redux";
+import { Input, Button } from "components";
+import { Link } from "react-router-dom";
 
-const SignIn: React.FC<Props> = () => {
-  return <div></div>;
+const SignInPage: React.FC = () => {
+  const dispatch = useDispatch();
+  const onFinish = (values: Store) => {
+    const { username, password } = values;
+    dispatch(
+      AccountActionCreators.signIn.request({
+        username,
+        password,
+      })
+    );
+  };
+
+  return (
+    <div
+      style={{
+        width: 436,
+        background: "#ffffff",
+        borderRadius: 20,
+        padding: 32,
+        margin: "auto",
+      }}
+    >
+      <Form onFinish={onFinish}>
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input placeholder="username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input type="password" placeholder="Password" />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            size="large"
+            style={{ width: "100%", marginTop: 24 }}
+          >
+            로그인
+          </Button>
+
+          <Button size="large" style={{ width: "100%", marginTop: 24 }}>
+            <Link to="/signup">회원가입</Link>
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
-export default SignIn;
+export default SignInPage;
